0dabfeb98c93069ac055699aa2e8edf7fc98bb60
ArrobaRicardoGE <ricardoarroba@outlook.com>
2022-02-18 14:04:19 -0600

    [MD engine] Experiment

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const { marked } = require('marked');
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/post/:post_id', (req, res) => {
+    fs.readFile(
+        path.join(__dirname, `public/${req.params.post_id}.md`),
+        'utf-8',
+        (err, data) => {
+            html = marked.parse(data);
+            res.send(html);
+        }
+    );
+});
+
+app.get('/dashboard/editor', (req, res) => {
     console.log(path.join(__dirname, '/index.html'));
     res.sendFile(path.join(__dirname, '/index.html'));
 });
